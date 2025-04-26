806d1ff3329e1191f3eae22e30678a49527a5c2e
keesrijpstra <keesrijpstrat@gmail.com>
2025-04-26 09:33:16 +0200

    blade settings

--- resources/views/livewire/dashboard/dashboard.blade.php
+++ resources/views/livewire/dashboard/dashboard.blade.php
@@ -2,10 +2,12 @@
     <div>
         <div class="lg:pl-72">
             <main class="py-10">
-                <div class=" px-4 sm:px-6 lg:px-8">
+                <div class="flex"></div>
+                <div class="px-4 sm:px-6 lg:px-8">
                     <span text-color>Welcome back {{ auth()->user()?->name . '!' ?? 'user!' }}</span>
                 </div>
             </main>
         </div>
     </div>
+    <div class=""></div>
 </x-layouts.dashboard>
