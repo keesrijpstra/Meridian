c369c0dad053b88a7764c3530995d40b0d1d5830
keesrijpstra <keesrijpstrat@gmail.com>
2025-04-26 09:18:14 +0200

    add formatting

--- resources/views/livewire/dashboard/dashboard.blade.php
+++ resources/views/livewire/dashboard/dashboard.blade.php
@@ -1,13 +1,13 @@
 <x-layouts.dashboard>
-<div>
-    <div class="lg:pl-72">
-      <main class="py-10">
-        <div class="px-4 sm:px-6 lg:px-8">
-            @if(request()->is('users'))
-                @livewire('users')
-            @endif
+    <div>
+        <div class="lg:pl-72">
+            <main class="py-10">
+                <div class="px-4 sm:px-6 lg:px-8">
+                    @if (request()->is('users'))
+                        @livewire('users')
+                    @endif
+                </div>
+            </main>
         </div>
-      </main>
     </div>
-</div>
 </x-layouts.dashboard>
